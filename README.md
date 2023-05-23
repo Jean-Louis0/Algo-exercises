001824f82a8848aea57b76902cc0e678e9303e80
root <root@JL-laptop.localdomain>
2023-05-23 09:40:47 +0200

    adding the readme and the first exercises(variables)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Algo-exercises-
+
+This is to do the algorithmics exercises of the junior web development class at Becode.
---
+++ variables.js
@@ -0,0 +1,46 @@
+/* The sum of two chosen numbers */
+
+// let a = 2;
+// let b = 3;
+
+// let sum = a + b;
+
+// console.log("The sum of a and b is:" );
+// console.log(sum);
+
+/* The division of two chosen numbers */
+
+
+// let a = 4;
+// let b = 2;
+
+// let division = a / b;
+
+// console.log("The result of the division of a and b is:" );
+// console.log(division);
+
+
+/* The modulo of two chosen numbers */
+
+// let a = parseFloat(prompt("Enter the first number please:"));
+// let b = parseFloat(prompt("Enter the second number please:"));
+
+// let modulo = a % b;
+
+// console.log("The modulo of the first and 2nd numbers is:" );
+// console.log(modulo);
+
+/* Function to calculate the modulo of two inputed numbers */
+
+// // Initialize readline
+// const readlineSync = require('readline-sync');
+// //input the values for the variables
+// let a = parseFloat(readlineSync.question('Enter the first number: '));
+// let b = parseFloat(readlineSync.question('Enter the second number: '));
+
+// let modulo = a % b;
+
+// console.log('The modulo of a and b is:');
+// console.log(modulo);
+
+
